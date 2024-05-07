a9a787ae78082eb1fde93f1386e3dc8ea5f6db13
SurajOberoi15 <shiv.oberoi@knoldus.com>
2024-05-07 18:18:34 +0530

    minor fixes

--- src/main/java/com/nashtech/service/SaveUploadedResumesService.java
+++ src/main/java/com/nashtech/service/SaveUploadedResumesService.java
@@ -2,6 +2,7 @@ package com.nashtech.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.api.core.ApiFuture;
+import com.google.cloud.http.HttpTransportOptions;
 import com.google.cloud.pubsub.v1.Publisher;
 import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
@@ -11,34 +12,40 @@ import com.google.protobuf.ByteString;
 import com.google.pubsub.v1.PubsubMessage;
 import com.google.pubsub.v1.TopicName;
 import com.nashtech.config.GCPConfig;
-import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.nio.charset.StandardCharsets;
+import java.io.ByteArrayInputStream;
 import java.time.Instant;
 import java.util.HashMap;
+import java.util.Objects;
 import java.util.UUID;
 
 @Service
-@AllArgsConstructor
+
 public class SaveUploadedResumesService {
     private final static Logger logger = LoggerFactory.getLogger(SaveUploadedResumesService.class);
 
     @Autowired
     private GCPConfig gcpConfig;
+    @Autowired
     private ObjectMapper objectMapper;
 
+    private static final String FILE_URL = "fileUrl";
+
+
+
     public void saveResumes(final MultipartFile file, String resumeData) {
+
         String uuid = String.valueOf(UUID.randomUUID());
         try {
-            //String path = sendToGCSBucket(file.getOriginalFilename(), resumeData);
+            String path = sendToGCSBucket(file.getOriginalFilename(), resumeData);
             HashMap<String, String> messageMap = new HashMap<>();
             messageMap.put("candidateID",uuid);
-            messageMap.put("path", "hello");
+            messageMap.put("path", path);
             messageMap.put("insertedTime", Instant.now().toString());
             messageMap.put("sourceSystem", "InternalStorage");
             pathPublisher(objectMapper.writeValueAsString(messageMap));
@@ -49,17 +56,29 @@ public class SaveUploadedResumesService {
     }
 
     private String sendToGCSBucket(String objectName, String resumeData) {
+        String fileUrl = "";
+
+        String baseUrl = "https://storage.cloud.google.com/";
+
         try {
-            System.out.println("line 57");
-            Storage storage = StorageOptions.newBuilder().setProjectId(gcpConfig.getGcpProjectId()).build().getService();
-            System.out.println("line 59");
-            BlobId blobId = BlobId.of(gcpConfig.getGcpBucketName(), objectName);
-            System.out.println("line 61");
-            BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("application/pdf").build();
-            System.out.println("line 63");
-            storage.create(blobInfo, resumeData.getBytes(StandardCharsets.UTF_8));
+            ByteString data = ByteString.copyFrom(Objects.requireNonNull(resumeData.getBytes()));
+
+            HttpTransportOptions transportOptions = StorageOptions.getDefaultHttpTransportOptions();
+            transportOptions = transportOptions.toBuilder().setConnectTimeout(60000).setReadTimeout(60000)
+                    .build();
+            Storage storage = StorageOptions.newBuilder().setTransportOptions(transportOptions)
+                    .setProjectId(gcpConfig.getGcpProjectId()).build().getService();
+
+            BlobId blobId = BlobId.of(Objects.requireNonNull(gcpConfig.getGcpBucketName()), objectName);
+            BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
+            byte[] content = data.toByteArray();
+            storage.createFrom(blobInfo, new ByteArrayInputStream(content));
+
+            fileUrl = baseUrl + gcpConfig.getGcpBucketName() + "/" + objectName;
+            logger.info(fileUrl);
+
             logger.info("uploaded to bucket " + gcpConfig.getGcpBucketName() + " as " + objectName);
-            return (gcpConfig.getGcpBucketName() + objectName);
+            return (fileUrl);
         } catch (Exception exception) {
             logger.info("Exception Occurred : "+ exception.getMessage());
             return ("Error");
