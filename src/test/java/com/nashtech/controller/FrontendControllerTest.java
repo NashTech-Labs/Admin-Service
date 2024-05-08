7c4fc595e8ff6395c0a9e5b85fe2727817eaf793
SurajOberoi15 <shiv.oberoi@knoldus.com>
2024-05-08 16:14:51 +0530

    Updated test-cases

--- src/test/java/com/nashtech/controller/FrontendControllerTest.java
+++ src/test/java/com/nashtech/controller/FrontendControllerTest.java
@@ -45,34 +45,12 @@ class FrontendControllerTest {
         assertTrue(actualTopCandidateProfile.getHeaders().isEmpty());
     }
 
+
     /**
      * Method under test: {@link FrontendController#getTopCandidateProfile()}
      */
     @Test
     void testGetTopCandidateProfile2() {
-
-        // Arrange
-        ReadFromResumesServices readFromResumesServices = new ReadFromResumesServices();
-        FrontendController frontendController = new FrontendController(null, readFromResumesServices,
-                new SaveUploadedResumesService());
-
-        // Act
-        ResponseEntity<String> actualTopCandidateProfile = frontendController.getTopCandidateProfile();
-
-        // Assert
-        assertEquals(500, actualTopCandidateProfile.getStatusCodeValue());
-        assertTrue(actualTopCandidateProfile.getHeaders().isEmpty());
-        String expectedBody = String.join("", "Error fetching the top candidates: Cannot invoke \"com.",
-                System.getProperty("user.name"),
-                ".config.LLMConfig.getTopCandidatesUrl()\" because \"this.llmConfig\" is null");
-        assertEquals(expectedBody, actualTopCandidateProfile.getBody());
-    }
-
-    /**
-     * Method under test: {@link FrontendController#getTopCandidateProfile()}
-     */
-    @Test
-    void testGetTopCandidateProfile3() {
 
 
         // Arrange
@@ -115,59 +93,13 @@ class FrontendControllerTest {
         assertTrue(actualUploadResumeDataResult.getHeaders().isEmpty());
     }
 
+
+
     /**
      * Method under test: {@link FrontendController#uploadResumeData(MultipartFile)}
      */
     @Test
     void testUploadResumeData2() throws IOException {
-
-        // Arrange
-        ReadFromResumesServices readFromResumesServices = new ReadFromResumesServices();
-        FrontendController frontendController = new FrontendController(null, readFromResumesServices,
-                new SaveUploadedResumesService());
-        MockMultipartFile file = new MockMultipartFile("Name", new ByteArrayInputStream("AXAXAXAX".getBytes("UTF-8")));
-
-        // Act
-        ResponseEntity<String> actualUploadResumeDataResult = frontendController.uploadResumeData(file);
-
-        // Assert
-        assertEquals(500, actualUploadResumeDataResult.getStatusCodeValue());
-        assertTrue(actualUploadResumeDataResult.getHeaders().isEmpty());
-        String expectedBody = String.join("", "Error processing Resume: Cannot invoke \"com.",
-                System.getProperty("user.name"),
-                ".config.LLMConfig.getResumeStructureUrl()\" because \"this.llmConfig\" is null");
-        assertEquals(expectedBody, actualUploadResumeDataResult.getBody());
-    }
-
-    /**
-     * Method under test: {@link FrontendController#uploadResumeData(MultipartFile)}
-     */
-    @Test
-    void testUploadResumeData3() throws IOException {
-
-        // Arrange
-        LLMConfig llmConfig = new LLMConfig();
-        FrontendController frontendController = new FrontendController(llmConfig, null, new SaveUploadedResumesService());
-        MockMultipartFile file = new MockMultipartFile("Name", new ByteArrayInputStream("AXAXAXAX".getBytes("UTF-8")));
-
-        // Act
-        ResponseEntity<String> actualUploadResumeDataResult = frontendController.uploadResumeData(file);
-
-        // Assert
-        assertEquals(500, actualUploadResumeDataResult.getStatusCodeValue());
-        assertTrue(actualUploadResumeDataResult.getHeaders().isEmpty());
-        String expectedBody = String.join("", "Error processing Resume: Cannot invoke \"com.",
-                System.getProperty("user.name"),
-                ".service.ReadFromResumesServices.readFromUploadedResumeFile(org.springframework.web.multipart.MultipartFile)\""
-                        + " because \"this.readFromResumesServices\" is null");
-        assertEquals(expectedBody, actualUploadResumeDataResult.getBody());
-    }
-
-    /**
-     * Method under test: {@link FrontendController#uploadResumeData(MultipartFile)}
-     */
-    @Test
-    void testUploadResumeData4() throws IOException {
 
         // Arrange
         ReadFromResumesServices readFromResumesServices = mock(ReadFromResumesServices.class);
@@ -188,38 +120,12 @@ class FrontendControllerTest {
         assertTrue(actualUploadResumeDataResult.getHeaders().isEmpty());
     }
 
-    /**
-     * Method under test: {@link FrontendController#uploadResumeData(MultipartFile)}
-     */
-    @Test
-    void testUploadResumeData5() throws IOException {
-
-        // Arrange
-        ReadFromResumesServices readFromResumesServices = mock(ReadFromResumesServices.class);
-        when(readFromResumesServices.readFromUploadedResumeFile(Mockito.<MultipartFile>any()))
-                .thenReturn("jane.doe@example.org");
-        FrontendController frontendController = new FrontendController(new LLMConfig(), readFromResumesServices, null);
-        MockMultipartFile file = new MockMultipartFile("Name", new ByteArrayInputStream("AXAXAXAX".getBytes("UTF-8")));
-
-        // Act
-        ResponseEntity<String> actualUploadResumeDataResult = frontendController.uploadResumeData(file);
-
-        // Assert
-        verify(readFromResumesServices).readFromUploadedResumeFile(isA(MultipartFile.class));
-        assertEquals(500, actualUploadResumeDataResult.getStatusCodeValue());
-        assertTrue(actualUploadResumeDataResult.getHeaders().isEmpty());
-        String expectedBody = String.join("", "Error processing Resume: Cannot invoke \"com.",
-                System.getProperty("user.name"),
-                ".service.SaveUploadedResumesService.saveResumes(org.springframework.web.multipart.MultipartFile,"
-                        + " String)\" because \"this.saveUploadedResumesService\" is null");
-        assertEquals(expectedBody, actualUploadResumeDataResult.getBody());
-    }
 
     /**
      * Method under test: {@link FrontendController#uploadResumeData(MultipartFile)}
      */
     @Test
-    void testUploadResumeData6() throws IOException {
+    void testUploadResumeData3() throws IOException {
 
         // Arrange
         ReadFromResumesServices readFromResumesServices = mock(ReadFromResumesServices.class);
@@ -246,7 +152,7 @@ class FrontendControllerTest {
      * Method under test: {@link FrontendController#uploadResumeData(MultipartFile)}
      */
     @Test
-    void testUploadResumeData7() throws IOException {
+    void testUploadResumeData4() throws IOException {
 
         // Arrange
         FrontendController frontendController = new FrontendController(new LLMConfig(), mock(ReadFromResumesServices.class),
